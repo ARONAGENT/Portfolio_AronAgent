a3ecc633ace69aaf5a545a85956264a6120f011a
Rohan Uke <ukerohan7@gmail.com>
2026-04-06 20:17:47 +0530

    feat : update BlogsPreview File name

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -3,7 +3,7 @@ import Hero from "@/components/Hero";
 import About from "@/components/About";
 import Skills from "@/components/Skills";
 import Projects from "@/components/Projects";
-import BlogsPreview from "@/components/BlogsPreview";
+import Blogspreview from "@/components/Blogspreview";
 import Certifications from "@/components/Certifications";
 import Resume from "@/components/Resume";
 import CurrentFocus from "@/components/CurrentFocus";
@@ -19,7 +19,7 @@ const Index = () => (
     <About />
     <Skills />
     <Projects />
-    <BlogsPreview />
+    <Blogspreview />
     <Certifications />
     <Resume />
     <CurrentFocus />
